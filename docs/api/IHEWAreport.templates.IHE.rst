5c9f49f2b4b350ac40b6e2903c21c604ec5a5979
Quan Pan <quanpan302@hotmail.com>
2020-03-25 09:46:53 +0100

    Clone from template

---
+++ docs/api/IHEWAreport.templates.IHE.rst
@@ -0,0 +1,38 @@
+IHEWAreport.templates.IHE package
+=================================
+
+Submodules
+----------
+
+IHEWAreport.templates.IHE.ADB module
+------------------------------------
+
+.. automodule:: IHEWAreport.templates.IHE.ADB
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+IHEWAreport.templates.IHE.FAO module
+------------------------------------
+
+.. automodule:: IHEWAreport.templates.IHE.FAO
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+IHEWAreport.templates.IHE.Manual module
+---------------------------------------
+
+.. automodule:: IHEWAreport.templates.IHE.Manual
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: IHEWAreport.templates.IHE
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ src/IHEWAreport/templates/IHE/Manual.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import inspect
+import os
+import yaml
+import numpy as np
+
+from pylatex import Document, Section, Subsection, Tabular, Math, TikZ, Axis, \
+    Plot, Figure, Matrix, Alignat
+from pylatex.utils import italic
+
+try:
+    # IHEClassInitError, IHEStringError, IHETypeError, IHEKeyError, IHEFileError
+    from .exception import IHEClassInitError
+except ImportError:
+    from IHEWAreport.exception import IHEClassInitError
+
+
+class Template(object):
+    """This Base class
+
+    Load base.yml file.
+
+    Args:
+        conf (dict): User defined configuration data from yaml file.
+    """
+    def __init__(self, conf):
+        """Class instantiation
+        """
+        template = 'Manual.yml'
+        path = os.path.join(
+            os.getcwd(),
+            os.path.dirname(
+                inspect.getfile(
+                    inspect.currentframe()))
+        )
+        print('{}.{}'.format(path, template))
+
+    def _conf(self, path, template):
+        pass
+
+    def create(self):
+        pass
+
+    def write(self):
+        pass
+
+    def saveas(self):
+        pass
+
+    def close(self):
+        pass
